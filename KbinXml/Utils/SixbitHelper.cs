f97bb8af5ef7fc9381c7f23b4c8b7719cf5d170f
Milkitic <Milkitic@126.com>
2022-02-05 17:22:59 +0800

    clean code

--- KbinXml/Utils/SixbitHelper.cs
+++ KbinXml/Utils/SixbitHelper.cs
@@ -30,34 +30,34 @@ public static class SixbitHelper
             : arrIn = ArrayPool<byte>.Shared.Rent(input.Length);
         try
         {
-        if (arrIn != null) buffer = buffer.Slice(0, input.Length);
-        for (var i = 0; i < input.Length; i++)
-        {
-            var c = input[i];
-            buffer[i] = CharsetMapping[c];
-        }
+            if (arrIn != null) buffer = buffer.Slice(0, input.Length);
+            for (var i = 0; i < input.Length; i++)
+            {
+                var c = input[i];
+                buffer[i] = CharsetMapping[c];
+            }
 
-        var length = (int)Math.Ceiling(buffer.Length * 6.0 / 8);
+            var length = (int)Math.Ceiling(buffer.Length * 6.0 / 8);
 
-        byte[]? arrOut = null;
-        var output = length <= Constants.MaxStackLength
-            ? stackalloc byte[length]
-            : arrOut = ArrayPool<byte>.Shared.Rent(length);
-        try
-        {
-        if (arrOut != null) output = output.Slice(0, length);
+            byte[]? arrOut = null;
+            var output = length <= Constants.MaxStackLength
+                ? stackalloc byte[length]
+                : arrOut = ArrayPool<byte>.Shared.Rent(length);
+            try
+            {
+                if (arrOut != null) output = output.Slice(0, length);
 
-        for (var i = 0; i < buffer.Length * 6; i++)
-            output[i / 8] = (byte)(output[i / 8] |
-                                   ((buffer[i / 6] >> (5 - (i % 6)) & 1) << (7 - (i % 8))));
+                for (var i = 0; i < buffer.Length * 6; i++)
+                    output[i / 8] = (byte)(output[i / 8] |
+                                           ((buffer[i / 6] >> (5 - (i % 6)) & 1) << (7 - (i % 8))));
 
-        var encode = output.Slice(0, output.Length);
-        stream.WriteSpan(encode);
-        }
-        finally
-        {
-            if (arrOut != null) ArrayPool<byte>.Shared.Return(arrOut);
-        }
+                var encode = output.Slice(0, output.Length);
+                stream.WriteSpan(encode);
+            }
+            finally
+            {
+                if (arrOut != null) ArrayPool<byte>.Shared.Return(arrOut);
+            }
         }
         finally
         {
@@ -73,35 +73,35 @@ public static class SixbitHelper
             : arrOutput = ArrayPool<byte>.Shared.Rent(length);
         try
         {
-        if (arrOutput != null) output = output.Slice(0, length);
+            if (arrOutput != null) output = output.Slice(0, length);
 
-        for (var i = 0; i < length * 6; i++)
-            output[i / 6] = (byte)(output[i / 6] |
-                                   (((buffer[i / 8] >> (7 - (i % 8))) & 1) << (5 - (i % 6))));
-        char[]? arrResult = null;
-        var result = output.Length <= Constants.MaxStackLength
-            ? stackalloc char[output.Length]
-            : arrResult = ArrayPool<char>.Shared.Rent(output.Length);
-        try
-        {
-        if (arrResult != null) result = result.Slice(0, output.Length);
+            for (var i = 0; i < length * 6; i++)
+                output[i / 6] = (byte)(output[i / 6] |
+                                       (((buffer[i / 8] >> (7 - (i % 8))) & 1) << (5 - (i % 6))));
+            char[]? arrResult = null;
+            var result = output.Length <= Constants.MaxStackLength
+                ? stackalloc char[output.Length]
+                : arrResult = ArrayPool<char>.Shared.Rent(output.Length);
+            try
+            {
+                if (arrResult != null) result = result.Slice(0, output.Length);
 
-        for (var i = 0; i < output.Length; i++)
-        {
-            var c = output[i];
-            result[i] = Charset[c];
-        }
+                for (var i = 0; i < output.Length; i++)
+                {
+                    var c = output[i];
+                    result[i] = Charset[c];
+                }
 
 #if NETSTANDARD2_1 || NETCOREAPP3_1_OR_GREATER
-        return new string(result);
+                return new string(result);
 #elif NETSTANDARD2_0
-        return new string(result.ToArray());
+                return new string(result.ToArray());
 #endif
-        }
-        finally
-        {
-            if (arrResult != null) ArrayPool<char>.Shared.Return(arrResult);
-        }
+            }
+            finally
+            {
+                if (arrResult != null) ArrayPool<char>.Shared.Return(arrResult);
+            }
         }
         finally
         {
