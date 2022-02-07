38ecb29f7deeb70c785fc490d33c2a7b6cd3e8bd
Milkitic <Milkitic@126.com>
2022-02-07 22:21:54 +0800

    revert changes
    This reverts commit ef5a0bba1aa87d548f1a4501e0596e560e6db0bc.

--- src/Benchmarks/WriteBenchmark-SingleThreadComparisonBetweenLibsTask-net6.0.ps1
+++ src/Benchmarks/WriteBenchmark-SingleThreadComparisonBetweenLibsTask-net6.0.ps1
@@ -1,4 +1,4 @@
 ﻿dotnet build WriteBenchmark --configuration Release --framework net6.0
 if ($LASTEXITCODE -eq 0) {
-  Start-Process -FilePath ./WriteBenchmark/bin/Release/net6.0/WriteBenchmark.exe -WorkingDirectory ./WriteBenchmark/bin/Release/net6.0 -NoNewWindow -Wait -ArgumentList "--filter *SingleThreadComparisonBetweenLibsTask*"
+  Start-Process ./WriteBenchmark/bin/Release/net6.0/WriteBenchmark.exe -WorkingDirectory ./WriteBenchmark/bin/Release/net6.0 -NoNewWindow -Wait -ArgumentList "--filter *SingleThreadComparisonBetweenLibsTask*"
 }
