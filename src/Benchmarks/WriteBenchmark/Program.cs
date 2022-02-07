d8d6c15feef53d570c3522b456b65ada3efaa5b1
Milkitic <Milkitic@126.com>
2022-02-07 18:03:06 +0800

    develop test

--- src/Benchmarks/WriteBenchmark/Program.cs
+++ src/Benchmarks/WriteBenchmark/Program.cs
@@ -1,4 +1,5 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System.Threading;
+using BenchmarkDotNet.Running;
 
 namespace WriteBenchmark;
 
@@ -6,6 +7,16 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        MultiThreadUtils.DoMultiThreadWork(i =>
+        {
+            var spin = new SpinWait();
+            for (int j = 0; j < 100; j++)
+            {
+                spin.SpinOnce();
+            }
+
+            return null;
+        }, 32, log: true);
         BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
     }
 }
