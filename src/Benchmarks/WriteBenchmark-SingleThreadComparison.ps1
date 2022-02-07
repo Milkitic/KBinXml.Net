23012ca90b10821536d9e82cd003c3c7dd5d0329
Milkitic <Milkitic@126.com>
2022-02-07 13:55:14 +0800

    update ps1

---
+++ src/Benchmarks/WriteBenchmark-SingleThreadComparison.ps1
@@ -0,0 +1,4 @@
+﻿dotnet build WriteBenchmark --configuration Release --framework net6.0
+if ($LASTEXITCODE -eq 0) {
+  Start-Process .\WriteBenchmark\bin\Release\net6.0\WriteBenchmark.exe -WorkingDirectory .\WriteBenchmark\bin\Release\net6.0 -NoNewWindow -Wait -ArgumentList "--filter *SingleThreadComparison*"
+}
