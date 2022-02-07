05292bf8f4ab04824c5da1f98932b2eed39ef54c
Milkitic <Milkitic@126.com>
2022-02-07 18:04:59 +0800

    fix path

--- src/Benchmarks/WriteBenchmark-MultiThreadComparisonBetweenLibsTask-net48.ps1
+++ src/Benchmarks/WriteBenchmark-MultiThreadComparisonBetweenLibsTask-net48.ps1
@@ -1,4 +1,4 @@
 ﻿dotnet build WriteBenchmark --configuration Release --framework net48
 if ($LASTEXITCODE -eq 0) {
-  Start-Process .\WriteBenchmark\bin\Release\net48\WriteBenchmark.exe -WorkingDirectory .\WriteBenchmark\bin\Release\net48 -NoNewWindow -Wait -ArgumentList "--filter *MultiThreadComparisonBetweenLibsTask*"
+  Start-Process ./WriteBenchmark/bin/Release/net48/WriteBenchmark.exe -WorkingDirectory ./WriteBenchmark/bin/Release/net48 -NoNewWindow -Wait -ArgumentList "--filter *MultiThreadComparisonBetweenLibsTask*"
 }
