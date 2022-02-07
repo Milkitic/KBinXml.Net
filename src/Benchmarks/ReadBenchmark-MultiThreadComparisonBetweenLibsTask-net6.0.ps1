0546e724f81c5dc3ce12354ce0fb90786964f77f
Milkitic <Milkitic@126.com>
2022-02-07 22:36:12 +0800

    compatible with linux

--- src/Benchmarks/ReadBenchmark-MultiThreadComparisonBetweenLibsTask-net6.0.ps1
+++ src/Benchmarks/ReadBenchmark-MultiThreadComparisonBetweenLibsTask-net6.0.ps1
@@ -1,4 +1,4 @@
 ﻿dotnet build ReadBenchmark --configuration Release --framework net6.0
 if ($LASTEXITCODE -eq 0) {
-  Start-Process .\ReadBenchmark\bin\Release\net6.0\ReadBenchmark.exe -WorkingDirectory .\ReadBenchmark\bin\Release\net6.0 -NoNewWindow -Wait -ArgumentList "--filter *MultiThreadComparisonBetweenLibsTask*"
+  Start-Process dotnet -WorkingDirectory ./ReadBenchmark/bin/Release/net6.0 -NoNewWindow -Wait -ArgumentList "ReadBenchmark.dll --filter *MultiThreadComparisonBetweenLibsTask*"
 }
