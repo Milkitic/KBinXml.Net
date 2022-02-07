befa8d7c17518b4df579c04904abd2e83994bafd
Milkitic <Milkitic@126.com>
2022-02-07 14:15:05 +0800

    ps1 files

---
+++ src/Benchmarks/ReadBenchmark-MultiThreadComparisonBetweenLibsTask-net48.ps1
@@ -0,0 +1,4 @@
+﻿dotnet build ReadBenchmark --configuration Release --framework net48
+if ($LASTEXITCODE -eq 0) {
+  Start-Process .\ReadBenchmark\bin\Release\net48\ReadBenchmark.exe -WorkingDirectory .\ReadBenchmark\bin\Release\net48 -NoNewWindow -Wait -ArgumentList "--filter *MultiThreadComparisonBetweenLibsTask*"
+}
