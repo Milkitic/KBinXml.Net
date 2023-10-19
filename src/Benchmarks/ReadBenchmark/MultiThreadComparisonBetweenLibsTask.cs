ebf348204869cfcb91e245d3d6d3d5be6a491829
Milkitic <Milkitic@126.com>
2023-10-19 11:47:20 +0800

    Update benchmarks

--- src/Benchmarks/ReadBenchmark/MultiThreadComparisonBetweenLibsTask.cs
+++ src/Benchmarks/ReadBenchmark/MultiThreadComparisonBetweenLibsTask.cs
@@ -19,7 +19,7 @@ namespace ReadBenchmark;
 [SimpleJob(RuntimeMoniker.Net48)]
 #else
 [SimpleJob(RuntimeMoniker.Net60)]
-[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+[SimpleJob(RuntimeMoniker.Net70)]
 #endif
 public class MultiThreadComparisonBetweenLibsTask
 {
